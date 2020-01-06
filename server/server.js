be13b5e1fe2e9d5c68897e38d77ba9a4b0cdfacb
Schweikofski <schweikofski@gmail.com>
2020-01-06 16:08:10 -0300

    Prepare Socket IO

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+
+let server = http.createServer(app);    // Uso la config de express para el server
+let io = socketIO(server);              // BackEnd Socket = mantiene el socket abierto
+
+// Escucho el server -> OJO. no es app, es server
+server.listen(port, (err) => {
+
+    if (err) throw new Error(err);
+
+    console.log(`Servidor corriendo en puerto ${ port }`);
+    console.log(' ');
+    console.log(' ');
+    console.log('DOCKER');
+    console.log(' ');
+    console.log('docker exec -it socket-io_app_node_1 sh --color=auto');
+
+});
